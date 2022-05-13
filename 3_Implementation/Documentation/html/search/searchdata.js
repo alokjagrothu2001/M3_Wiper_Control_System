a94d8c4b0f06a10593626eac5fb7bb49d96eb2d3
alokjagrothu2001 <101423374+alokjagrothu2001@users.noreply.github.com>
2022-05-13 23:24:27 +0530

    Add files via upload

---
+++ 3_Implementation/Documentation/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "_egmrs",
+  1: "egr",
+  2: "ms",
+  3: "_"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions"
+};
+
